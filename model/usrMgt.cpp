8d80e71596fa8ee251bfa265290ea821f7a9377c
GhostWarlock <shawn-woo@qq.com>
2021-02-13 12:34:41 +0800

    buid: modify CMakeLists

---
+++ model/usrMgt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wusong on 2021/2/13.
+//
+
+#include "usrMgt.h"
---
+++ model/usrMgt.h
@@ -0,0 +1,14 @@
+//
+// Created by wusong on 2021/2/13.
+//
+
+#ifndef MINI_MSG_USRMGT_H
+#define MINI_MSG_USRMGT_H
+
+
+class usrMgt {
+
+};
+
+
+#endif //MINI_MSG_USRMGT_H
